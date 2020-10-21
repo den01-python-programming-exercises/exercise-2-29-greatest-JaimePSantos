7a6370000e75f129f001448c9d613d260b4a0fde
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 19:51:24 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
+def greatest(number1, number2, number3):
+    # write your code here
+    if(number1>number2):
+      if(number1>number3):
+        return number1
+      else:
+        return number3
+    elif(number2 > number1):
+      if(number2>number3):
+        return number2
+      else:
+        return number3
+        
 def main():
-    #write your code below this line
+    answer = greatest(2, 7, 9)
+    print("Greatest: " + str(answer))
 
 if __name__ == '__main__':
     main()
